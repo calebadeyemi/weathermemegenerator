a0171b08c59a22e9b7f2771541f42b53f4b5b1c0
henningfutrell <henning.futrell@protonmail.com>
2019-04-22 16:09:15 -0500

    Add classes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
+import Meme from './Meme';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-            <h1>Generate an image.</h1>
-            <input type={"text"}  pattern="[0-9]*"/>
-            <button> Generate! </button>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props)
+        this.state = { generatedMeme: null, zipcode: null }
+    }
+
+    handleGenerateMeme() {
+        this.setState({ generatedMeme: <div>hi</div> } )
+    }
+
+    handleReset() {
+        this.setState({ generatedMeme: null, zipcode: null })
+    }
+
+    render() {
+        return (
+            <div className="Body">
+                <div className="Form">
+                    <h1>Generate a Weather Meme</h1>
+                    <p>Enter a zip-code</p>
+                    <input type="text" className="ZipBox"  pattern="[0-9][0-9][0-9][0-9][0-9]"/>
+                    <button className="Button" onClick={this.handleGenerateMeme}>Generate!</button>
+                </div>
+                <div className="Presenter">
+                    {this.state.generatedMeme && <button className="Button" onClick={this.handleReset}>reset!</button>}
+                </div>
+                <footer className="Footer">
+                    <a  className="Link" href="https://darksky.net/poweredby/">Powered by Dark Sky</a>
+                </footer>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Meme.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Meme extends Component {
+    render() {
+        return(<div><p className="Meme">I'm a Meme!</p></div>)
+    }
+}
+
+export default Meme;
