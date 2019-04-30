cbb06de1e99e5c8bfdc3157e8e8081b029a4345e
henningfutrell <henning.futrell@protonmail.com>
2019-04-30 11:03:30 -0500

    Sanitation work

--- src/keys.js
+++
@@ -1,7 +0,0 @@
-const keys = {
- google_key : 'AIzaSyDjHzLbf6xrTtjRojts48bhusehdljG6Vs',
- dark_sky_key : '1e0ee3b964e62f67037b3566eee15410',
- giphy_key : 'NgplQXmqoQEVvceyuKW8vlB7IgcYjxVK'
-}
-
-module.exports = keys;
